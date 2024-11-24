ebc4b8862ea78f616f65d92b3da6571730a5c523
Malu38 <maelou3105@gmail.com>
2024-11-24 08:17:41 +0100

    redo everything

--- TimeChanger/settings.lua
+++ TimeChanger/settings.lua
@@ -1,14 +1,14 @@
 data:extend({
     {
         type = "int-setting",
-        name = "time-changer-max-time-speed",
+        name = "max-time-speed",
         setting_type = "runtime-global",
         default_value = 64,
         allowed_values = {2, 4, 8, 16, 32, 64, 128, 256}
     },
     {
         type = "int-setting",
-        name = "time-changer-min-time-speed",
+        name = "min-time-speed",
         setting_type = "runtime-global",
         default_value = 16,
         allowed_values = {2, 4, 8, 16}
